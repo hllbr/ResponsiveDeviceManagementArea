47a43c43977b9002ca63aee13df2f74a7f044ed6
hikocak <hikocak@netas.com.tr>
2025-07-21 11:41:18 +0300

    Adjust height calculation in DeviceScreen for improved layout responsiveness; modify Home component structure to ensure full height utilization and enhance visual alignment.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -16,15 +16,15 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 grid grid-cols-[auto_1fr]">
+    <div className="h-screen bg-gray-100 grid grid-cols-[auto_1fr]">
       <Sidebar onDeviceSelect={setSelectedDeviceSrc} />
       {/* Main Area (Header + Content) */}
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="flex flex-col min-w-0 h-full flex-1 h-0">
         <div className="flex-shrink-0 px-4 lg:px-8 pt-4 lg:pt-8">
           <DeviceDetails />
         </div>
         {/* Main Content Row */}
-        <main className="grid grid-cols-1 xl:grid-cols-[auto_1fr] gap-8 px-4 lg:px-8 pb-4 lg:pb-8 flex-1 items-stretch h-full">
+        <main className="grid grid-cols-1 xl:grid-cols-[auto_1fr] gap-8 px-4 lg:px-8 pb-4 lg:pb-8 flex-1 h-0 items-stretch">
           {/* Sol Panel - DeviceArea */}
           <div className="flex flex-col h-full min-w-0">
             <DeviceArea
